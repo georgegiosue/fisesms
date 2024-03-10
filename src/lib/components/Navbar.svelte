17ea8ee3f25184f7efd3356309b0f49e1ff54e50
georgegiosue <peraldonamoc@gmail.com>
2024-03-10 15:54:00 -0500

    refactor: Navbar links visibility

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -5,22 +5,22 @@
 
 <nav class="flex items-center justify-between">
 	<ul
-		class="flex space-x-4 rounded-lg p-2 ring-1 ring-foreground/15 [&>li]:flex [&>li]:items-center [&>li]:rounded-lg [&>li]:p-2 [&>li]:font-semibold [&>li]:transition-colors [&>li]:duration-200"
+		class="flex space-x-4 rounded-lg px-2 py-1 ring-1 ring-foreground/15 [&>li]:flex [&>li]:items-center [&>li]:rounded-lg [&>li]:p-2 [&>li]:font-semibold [&>li]:transition-colors [&>li]:duration-200"
 	>
 		<li class="hover:bg-foreground/10">
 			<a href="/">
-				Inicio <Menubar.Shortcut class="text-md ml-2">⌘H</Menubar.Shortcut>
+				Inicio <Menubar.Shortcut class="text-md ml-2 hidden lg:inline">⌘H</Menubar.Shortcut>
 			</a>
 		</li>
 		<li class="hover:bg-foreground/10">
 			<a href="/privacy">
 				Politica de P.
-				<Menubar.Shortcut class="text-md ml-2">⌘P</Menubar.Shortcut>
+				<Menubar.Shortcut class="text-md ml-2 hidden lg:inline">⌘P</Menubar.Shortcut>
 			</a>
 		</li>
 		<li class="hover:bg-foreground/10">
 			<a href="/faq">
-				FAQ <Menubar.Shortcut class="text-md ml-2">⌘A</Menubar.Shortcut>
+				FAQ <Menubar.Shortcut class="text-md ml-2 hidden lg:inline">⌘A</Menubar.Shortcut>
 			</a>
 		</li>
 	</ul>
