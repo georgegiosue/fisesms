609c01abf193373621821e26745e43580da6841c
georgegiosue <peraldonamoc@gmail.com>
2024-03-10 15:44:16 -0500

    refactor: Navbar component with links and theme toggle

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+	import * as Menubar from '$lib/components/ui/menubar';
+</script>
+
+<nav class="flex items-center justify-between">
+	<ul
+		class="flex space-x-4 rounded-lg p-2 ring-1 ring-foreground/15 [&>li]:flex [&>li]:items-center [&>li]:rounded-lg [&>li]:p-2 [&>li]:font-semibold [&>li]:transition-colors [&>li]:duration-200"
+	>
+		<li class="hover:bg-foreground/10">
+			<a href="/">
+				Inicio <Menubar.Shortcut class="text-md ml-2">⌘H</Menubar.Shortcut>
+			</a>
+		</li>
+		<li class="hover:bg-foreground/10">
+			<a href="/privacy">
+				Politica de P.
+				<Menubar.Shortcut class="text-md ml-2">⌘P</Menubar.Shortcut>
+			</a>
+		</li>
+		<li class="hover:bg-foreground/10">
+			<a href="/faq">
+				FAQ <Menubar.Shortcut class="text-md ml-2">⌘A</Menubar.Shortcut>
+			</a>
+		</li>
+	</ul>
+	<ThemeToggle />
+</nav>
