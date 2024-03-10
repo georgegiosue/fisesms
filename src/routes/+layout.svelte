7995021c88bdc86651342f15685af2cc48366d68
georgegiosue <peraldonamoc@gmail.com>
2024-03-10 01:05:30 -0500

    fix: keyboard event handling in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,43 +2,53 @@
 	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
 	import * as Menubar from '$lib/components/ui/menubar';
 	import { ModeWatcher } from 'mode-watcher';
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 	import '../app.pcss';
 
 	onMount(() => {
-    function handleKeydown(e: KeyboardEvent) {
-		e.preventDefault();
-		const key = e.key;
+		function handleKeydown(e: KeyboardEvent) {
+			e.preventDefault();
+			const key = e.key;
 
-		switch (key) {
-			case "p":
-			case "P":
-				window.location.href = "/privacy";
-				break;
-			case "a":
-			case "A":
-				window.location.href = "/faq";
-				break;
+			const currentHostname = window.location.hostname;
+			const currentProtocol = window.location.protocol;
+			const currentPort = window.location.port ? `:${window.location.port}` : '';
 
-			case "h":
-			case "H":
-				window.location.href = "/";
-				break;
+			switch (key) {
+				case 'p':
+				case 'P':
+					const redirectUrlPrivacy = `${currentProtocol}//${currentHostname}${currentPort}/privacy`;
+					window.location.href = redirectUrlPrivacy;
+					break;
+				case 'a':
+				case 'A':
+					const redirectUrlFaq = `${currentProtocol}//${currentHostname}${currentPort}/faq`;
+					window.location.href = redirectUrlFaq;
+					break;
+
+				case 'h':
+				case 'H':
+					const redirectUrlHome = `${currentProtocol}//${currentHostname}${currentPort}/`;
+					window.location.href = redirectUrlHome;
+					break;
+			}
 		}
-    }
- 
-    document.addEventListener("keydown", handleKeydown);
-    return () => {
-      document.removeEventListener("keydown", handleKeydown);
-    };
-  });
+
+		document.addEventListener('keydown', handleKeydown);
+		return () => {
+			document.removeEventListener('keydown', handleKeydown);
+		};
+	});
 </script>
 
 <ModeWatcher />
 
 <svelte:head>
 	<title>Fise SMS</title>
-	<meta name="description" content="Aplicación de Android que permite a los proveedores de GLP (Gas Licuado de Petróleo) procesar cupones de descuento" />
+	<meta
+		name="description"
+		content="Aplicación de Android que permite a los proveedores de GLP (Gas Licuado de Petróleo) procesar cupones de descuento"
+	/>
 </svelte:head>
 
 <main class="m-2 rounded-lg p-4 lg:m-24 lg:mx-48">
@@ -55,7 +65,7 @@
 						Politica de Privacidad
 						<Menubar.Shortcut class="text-md ml-2">⌘P</Menubar.Shortcut>
 					</a>
-					</Menubar.Trigger>
+				</Menubar.Trigger>
 			</Menubar.Menu>
 			<Menubar.Menu>
 				<Menubar.Trigger>
